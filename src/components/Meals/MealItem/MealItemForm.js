2974fd3231d2a9ea020512fefc46fd9df07760c4
aman-s1 <amansuhag6014@gmail.com>
2024-05-18 10:49:48 +0530

    Cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,15 +4,18 @@ import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
     return (
-        <form className={classes.form} >
-            <Input label="Amount" input={{
-                id: 'amount',
+        <form className={classes.form}>
+            <Input
+                label='Amount'
+                input={{
+                id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',
                 max: '5',
                 step: '1',
-                defaultValue: '1'
-            }} />
+                defaultValue: '1',
+                }}
+            />
             <button>+ Add</button>
         </form>
     );
